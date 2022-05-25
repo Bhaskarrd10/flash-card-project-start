569320430b5d9233fcd27b789bfda05371e37793
Bhaskarrd10 <bhaskarrd10@gmail.com>
2022-05-25 08:12:41 +0530

    adding flip_card func

--- main.py
+++ main.py
@@ -6,22 +6,35 @@ import random
 
 data= pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    current_card["French"]
 
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
 
 window = Tk()
 window.title("Flash_card")
 window.config(padx=50, pady=50, bg= BACKGROUND_COLOR)
 
+flip_timer =window.after(3000, func=flip_card)
 
 canvas= Canvas(width=800, height=600)
 card_front_image = PhotoImage(file='images/card_front.png')
-canvas.create_image(400, 265, image=card_front_image)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "bold"))
-canvas.create_text(400, 250, text="word", font=("Ariel", 40, "italic"))
+card_back_image = PhotoImage(file='images/card_back.png')
+card_background = canvas.create_image(400, 265, image=card_front_image)
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "bold"))
+card_word = canvas.create_text(400, 250, text="word", font=("Ariel", 40, "italic"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -33,7 +46,7 @@ right_image = PhotoImage(file="images/right.png")
 known_button = Button(image=right_image, highlightthickness=0, command= next_card)
 known_button.grid(row=1, column=1)
 
-
+next_card()
 
 
 
